5e56267461edd114242b8d8b4c28a7c882ce7062
Wocx32 <63423836+Wocx32@users.noreply.github.com>
2024-10-05 03:53:44 +0500

    add limits to login page inputs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,8 @@
 <h1>Sign in</h1>
 <form method="post" use:enhance>
     <label for="username">Username</label>
-    <input name="username" id="username" /><br />
+    <input name="username" id="username" minlength="3" maxlength="31" /><br />
     <label for="password">Password</label>
-    <input type="password" name="password" id="password" /><br />
+    <input type="password" name="password" id="password" minlength="6" maxlength="255" /><br />
     <button>Continue</button>
 </form>
