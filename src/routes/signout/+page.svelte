03fbf44d97a9a95555a8059576d36fea1613f2b0
Wocx32 <63423836+Wocx32@users.noreply.github.com>
2024-10-05 03:51:57 +0500

    add signout, add min and max length to username password

---
+++ src/routes/signout/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { enhance } from "$app/forms";
+</script>
+
+<h1>Sign out</h1>
+<form method="post" use:enhance>
+    <button class="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Sign out</button>
+</form>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -6,8 +6,8 @@
 
 <form method="post" use:enhance>
     <label for="username">Username</label>
-    <input name="username" id="username" /><br />
+    <input name="username" id="username" minlength="3" maxlength="31" /><br />
     <label for="password">Password</label>
-    <input type="password" name="password" id="password" /><br />
+    <input type="password" name="password" id="password" minlength="6" maxlength="255" /><br />
     <button>Continue</button>
 </form>
